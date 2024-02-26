cc19e558540d0a1f664874a716e42d62ab43ab29
Till Kleisli <till@kleisli.ch>
2024-02-26 16:59:09 +0100

    FEATURE add translate for Gedmo translatable entities

---
+++ Classes/Controller/TranslateControllerTrait.php
@@ -0,0 +1,55 @@
+<?php
+namespace Milly\CrudForms\Controller;
+
+use Gedmo\Translatable\TranslatableListener;
+use Neos\Flow\Annotations as Flow;
+
+trait TranslateControllerTrait
+{
+
+    #[Flow\InjectConfiguration(path: "defaultLocale", package: "Sandstorm.GedmoTranslatableConnector")]
+    protected $defaultLocale;
+
+    #[Flow\Inject]
+    protected TranslatableListener $translatableListener;
+
+    protected function initializeTranslateAction()
+    {
+        $this->registerObjectArgument();
+    }
+
+    public function translateAction(?string $locale = null)
+    {
+        $object = $this->arguments['object']->getValue();
+        if($locale) {
+            $object->reloadInLocale($locale);
+        }
+        $this->view->assign('object', $object);
+
+        $this->view->assign('locale', $locale);
+        $this->view->assign('configuration', $this->getCrudFormsConfiguration('translate'));
+        $this->view->assign('crudFormsModelClass', $this->getModelClass());
+    }
+
+    protected function initializeUpdateTranslationAction()
+    {
+        $this->registerObjectArgument();
+    }
+
+    public function updateTranslationAction(string $locale)
+    {
+        $this->translatableListener->setTranslatableLocale($locale);
+
+        $object = $this->arguments['object']->getValue();
+
+        $this->getRepository()->update($object);
+
+        if (method_exists($this, 'afterUpdateTranslationAction')) {
+            $this->afterUpdateTranslationAction($object);
+        }
+
+        $this->redirect('translate', null, null, ['object' => $object, 'locale' => $locale]);
+
+    }
+
+}
